d801a2b1d274ccc51a448761cda7f1910dc3ace8
tragsdale7 <39227317+tragsdale7@users.noreply.github.com>
2018-12-27 16:40:10 -0500

    Start find a pet section and gather breed images

--- main1.js
+++ main1.js
@@ -1,27 +1,4 @@
-
-let data;
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-	if(xhr.readyState == XMLHttpRequest.DONE) {
-		console.log(xhr.responseText)
-		let json = xhr.responseText;
-		data = JSON.parse(json);
-
-		console.log(data.message);
-
-		for (var property in data.message) {
-		    if (data.message.hasOwnProperty(property)) {
-		       console.log(property);
-		    }
-		}	
-	}
+function displayInitialBreeds() {
+	
 }
-
-xhr.open('GET', 'https://dog.ceo/api/breeds/list/all', true);
-xhr.send(null);
-
-
-
-
-
 
